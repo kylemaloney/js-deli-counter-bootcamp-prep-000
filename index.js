79444ca9b75d02cb1594989c3ec98158378b1005
kylemaloney17@gmail.com <kylemaloney17@gmail.com>
2018-08-25 21:01:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function takeANumber (currentLine, name){
+  currentLine.push(name);
+  var number = currentLine.length;
+  return ("Welcome, " + name + ". You are number " + number + " in line.");
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length > 0){
+    name = katzDeliLine.shift();
+    return ("Currently serving " + name + ".");
+  } else {
+    return ("There is nobody waiting to be served!")
+  }
+}
+
+function currentLine (line){
+  if (line.length > 0){
+    var string = "The line is currently: "; 
+      for (var i = 0; i < line.length; i++){
+        string += ((i+1) + ". " + line.i + " ");
+        return string;
+    } 
+  } else {
+    return ("The line is currently empty.");
+  }
+}
